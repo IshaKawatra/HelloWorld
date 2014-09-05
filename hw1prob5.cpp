5cb6d99702310fab23d42b0878380bda4d36d462
Isha Kawatra <kawatra@usc.edu>
2014-09-05 11:03:25 -0700

    hw1 problem 5

--- hw1prob5.cpp
+++ hw1prob5.cpp
@@ -10,6 +10,9 @@ int main (int argc, char* argv[]){
   ifstream inputfile;
   filename = argv[1];
   inputfile.open(filename.c_str()); //read in file
+  if (inputfile.fail()){
+	cout << "Error: invalid file " << endl;
+  }
   string STRING1;
   getline(inputfile,STRING1); 
   stringstream s1;		
@@ -33,25 +36,25 @@ int main (int argc, char* argv[]){
 	int whichbucket, howmany;
 	ss >> command >> whichbucket;
 	if(command == "PUT"){ //if it directs to fill the bucket
-		if (whichbucket <= numBuckets){
-			if (empty[whichbucket-1]==0){	
+		if ((whichbucket <= numBuckets) && (whichbucket > 0)){
+			if (empty[whichbucket]==0){	
 				ss >> howmany;
 				bucketsize[whichbucket] = howmany;	
 				*(buckets+whichbucket) = new int[howmany];
 				for (int n = 0; n < howmany; n++){
 					ss >> buckets[whichbucket][n];
 					}
-				empty[whichbucket-1] = 1; //empty array indices are off by one			
+				empty[whichbucket] = 1; //no longer empty			
 				}
 			else{ 
-				cout << "Error: bucket isn't empty" << endl;
+				cout << "Error: bucket " << whichbucket << " isn't empty" << endl;
 				}
 		}
 		else
 			{cout << "Error" << endl;}
 	}
 	else if(command == "OUTPUT"){ //if it directs to output contents of bucket
-		if ((whichbucket <= numBuckets) && (whichbucket > 0) && (empty[whichbucket-1] == 1))	
+		if ((whichbucket <= numBuckets) && (whichbucket > 0) && (empty[whichbucket] == 1))	
 		{	
 			for (int c = 0; c < bucketsize[whichbucket]; c++){
 				
@@ -59,32 +62,37 @@ int main (int argc, char* argv[]){
 			}
 			cout << endl;
 		}
-		else if ((whichbucket <= numBuckets) && (whichbucket > 0) && (empty[whichbucket-1] == 0))
+		else if ((whichbucket <= numBuckets) && (whichbucket > 0) && (empty[whichbucket] == 0))
 		{
-			cout << "Error: bucket is empty" << endl;
+			cout << "empty" << endl;
 		} 
 		else
 		{
-			cout << "Error: bucket does not exist" << endl;
+			cout << "Error: bucket " << whichbucket << " does not exist" << endl;
 		}
 	}
 	else if (command == "EMPTY") //if it directs to empty a bucket
 	{
 		if (whichbucket <= numBuckets)
-		{			
-			(empty[whichbucket-1]) = 0;
-			delete [] buckets[whichbucket];
-		}
+		{
+		   if(empty[whichbucket] == 1){			
+			(empty[whichbucket]) = 0;
+			//delete [] buckets[whichbucket]; //deallocate
+			//buckets[whichbucket] = NULL; //set the pointer to null
+		}}
 		else
 		{
 			cout << "Error" << endl;
 		}
 			
-		}		
+	}		
   }
-for (int l = 0; l < numBuckets; l++){
-	delete [] buckets[l];
+
+for (int l = 0; l < numBuckets; l++){ //deallocate
+	if (empty[l] == 1){
+	delete [] buckets[l];}
 }
+
 delete [] buckets;
 return 0;
 
